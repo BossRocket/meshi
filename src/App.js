d7ad61956acda55f0ac07a60bd1959b5a4dc91d7
Ryan <patrickrmaley@gmail.com>
2021-07-16 00:41:38 -0400

    Updated ETA

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-		  Under construction. ETA August 22, 2021.
+		  Under construction. ETA Fall 2021.
         </p>
         <a
           className="App-link"
